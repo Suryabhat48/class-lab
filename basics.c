9f66be6304c2b00430dfa8973a0855a0b889e2ca
Suryabhat48 <suryabhat2032@gmail.com>
2025-08-08 12:08:05 +0530

    Add new c program

---
+++ basics.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+   int a,b,temp;
+   int *p1,*p2;
+   printf("Enter two numbers:");
+   scanf("%d %d",&a,&b);
+   p1=&a;
+   p2=&b;
+   temp=*p1;
+   *p1=*p2;
+   *p2=temp;
+   printf("After swapping the numbers are :");
+   printf("a=%d b=%d",a,b);
+   return 0;
+}
